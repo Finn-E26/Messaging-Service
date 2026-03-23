0b91c02a56e2ec834020573a5a2eb1dafff0887e
Finn-E26 <finn.e_08@outlook.com>
2026-03-23 16:55:33 -0400

    queue testing

--- api.js
+++ api.js
@@ -176,12 +176,12 @@ async function getQueuedMessages(webSocket) {
     let response = await pool.query("SELECT * FROM messages WHERE receiver = $1 AND delivered = $2",[username,false]);
 
     if (response.rowCount > 0) {
-        for (i = 0; i<rowCount; i++) {
+        for (let i = 0; i<rowCount; i++) {
             console.log(JSON.stringify(rows[i]));
         }
     }
     console.log("Received response from db");
-    console.log(response);
+    //console.log(response);
 }
 
 async function verifyCredentials(type, username, password, ws) {
